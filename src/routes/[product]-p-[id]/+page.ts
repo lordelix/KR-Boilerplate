433c2c539b84f171a7c31fcd8235177827f04224
Felix Heidecke <felix@heidecke.me>
2025-12-02 11:47:07 +0100

    removing group data retrieval on PDP

--- src/routes/[product]-p-[id]/+page.ts
+++ src/routes/[product]-p-[id]/+page.ts
@@ -1,19 +1,13 @@
-import { useGroups } from '$lib/boilerplate/xioni/shop/Groups.js'
 import { useProducts } from '$lib/boilerplate/xioni/shop/Products.js'
 import xioniLoader from '$lib/boilerplate/xioni/utils/xioniLoader'
 
 const { getProduct } = useProducts()
-const { getGroupByProductId } = useGroups()
 
 export async function load({ params }) {
 	const id = +params.id
-	const [product, group] = await Promise.all([
-		xioniLoader(getProduct(id)),
-		xioniLoader(getGroupByProductId(id))
-	])
+	const product = await xioniLoader(getProduct(id))
 
 	return {
-		product,
-		group
+		product
 	}
 }
